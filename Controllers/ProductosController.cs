19b0d3de5392cf3b69e62b874c0f47a41193ebf9
Enmanuel <enmanuelbrosa07@gmail.com>
2026-08-30 19:35:01 +0200

    Agrega endpoint GET api/Productos que retorna datos en formato Json

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiApiCuadrado.Data;
+
+namespace MiApiCuadrado.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductosController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProductosController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTodos()
+    {
+        var productos = await _context.Productos.ToListAsync();
+        return Ok(productos);
+    }
+}
