77dc3c20e693b86ce3bb479f5e6b6da1f3d1189a
Enmanuel <enmanuelbrosa07@gmail.com>
2026-08-30 19:32:21 +0200

    Agrega AppDbContext para conexion con base de datos

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiApiCuadrado.Models;
+
+namespace MiApiCuadrado.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Producto> Productos { get; set; }
+}
